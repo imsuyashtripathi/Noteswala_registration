3ca7d3b93b3816a19156b9507544bbba3207fa2b
Suyash tripathi <39858997+imsuyashtripathi@users.noreply.github.com>
2020-04-07 10:14:36 +0530

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html  >
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.12.3, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/logo-1.png" type="image/x-icon">
+  <meta name="description" content="Website Creator Description">
+  
+  
+  <title>upload</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="preload" as="style" href="assets/mobirise/css/mbr-additional.css"><link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+  <section class="menu cid-rUSVEQW6AW" once="menu" id="menu1-3w">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="index.html">
+                         <img src="assets/images/logo-1.png" alt="Mobirise" title="" style="height: 4.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-white display-4" href="index.html"><span class="mbri-arrow-next mbr-iconfont mbr-iconfont-btn"></span></a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown nav-right" data-app-modern-menu="true"><li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="notes.html"><span class="mbri-edit mbr-iconfont mbr-iconfont-btn"></span>
+                        Notes
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link link text-white display-4" href="papers.html"><span class="mbri-file mbr-iconfont mbr-iconfont-btn"></span>
+                        Papers
+                    </a>
+                </li></ul>
+            
+        </div>
+    </nav>
+</section>
+<section class="header3 cid-rUT4tYLnG0 mbr-fullscreen mbr-parallax-background" id="header3-4">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.4; background-color: rgb(70, 80, 82);">
+    </div>
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="media-content">
+                <h1 class="mbr-section-title mbr-white align-center mbr-bold pb-3 mbr-fonts-style display-1">Noteswala</h1>
+                <h3 class="mbr-section-subtitle mbr-white align-center mbr-light pb-3 mbr-fonts-style display-2">Thankyou</h3>
+                <div class="mbr-section-text mbr-white pb-3 ">
+                <p class="mbr-text align-center pb-3 mbr-fonts-style display-7">&nbsp;For Uploading the notes or papers<br>it would help alot of student for Prepareing their semester exams<br> as well as competitive exams</p></div>
+                <div class="mbr-section-btn align-center">
+				<a class="btn btn-md btn-primary display-4" href="https://mobirise.co">
+                        <span class="mbr-iconfont mbri-file"></span>Click to Upload file</a></div>
+            </div>
+        </div>
+    </div>
+
+</section>
+
+
+<section class="cid-rUTqsqiqcy" id="footer1-u">
+        <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                    <a href="index.html">
+                        <img src="assets/images/logo.png" alt="Mobirise" title="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Address
+                </h5>
+                <p class="mbr-text">Badlapur Jaunpur&nbsp;<br>Uttar Pradesh, &nbsp;222175</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Contacts Us</h5>
+                <p class="mbr-text">
+                    Email: suyashtripathi98@gmail.com
+                    <br>Phone: +91 &nbsp; 8574266136&nbsp;<br>Fax: +91 &nbsp; 9889071916</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Links
+                </h5>
+                <p class="mbr-text"><a href="aboutus.html"><font color="white">About Us</font></a><br></p>
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        © Copyright 2019 Noteswala - All Rights Reserved
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <div class="social-list align-right">
+                        <div class="soc-item">
+                            <a href="https://twitter.com/tripathisuyash" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-twitter socicon"></span>
+                            </a>
+                        </div>
+                        <div class="soc-item">
+                            <a href="https://www.facebook.com/suyash.tripathi.5070" target="_blank">
+                                <span class="mbr-iconfont mbr-iconfont-social socicon-facebook socicon"></span>
+                            </a>
+                        </div>
+                        
+                        
+                        
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/dropdown/js/nav-dropdown.js"></script>
+  <script src="assets/dropdown/js/navbar-dropdown.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  
+  
+</body>
+</html>
